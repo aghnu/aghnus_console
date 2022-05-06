9c059eb6e6742fa5c16daed63c99c33282729bef
Gengyuan Huang <gengyuan@ualberta.ca>
2022-05-06 06:39:33 -0600

    Update welcome msg

--- src/textList.js
+++ src/textList.js
@@ -7,17 +7,14 @@ function PJ(type, param={}) {
 
 // text lists
 export const welcomePrintingJobTextList = [
-    PJ("text", {text: "Hello stranger!"}),
-    PJ("text", {text: "Welcome... Welcome..."}),
+    PJ("text", {text: "Hello stranger!<br>Welcome... Welcome..."}),
     PJ("text", {text: "Name is Gengyuan Huang<br>A programmer..."}),
     PJ("line", {height: 2}),
 
     PJ("text", {text: "I have recently graduated from the University with a CS degree...<br>I have rent to pay, and a mouth to feed (my mouth)..."}),
     PJ("line", {height: 2}),
 
-    PJ("text", {text: "if you wish to know more about me, my life or my cat..."}),
-    PJ("text", {text: "or my social insurance number"}),
-    PJ("text", {text: "please type /help"}),
+    PJ("text", {text: "if you wish to know more about me, my life or my cat...<br>or my social insurance number<br>please type /help"}),
     PJ("line", {height: 2}),
 
     PJ("text", {text: "To contact me:"}),
