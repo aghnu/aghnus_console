87af91e0bc0e69cc3a968ed53050d13a1e0da80a
Gengyuan Huang <scotthuang007@outlook.com>
2023-02-05 21:48:07 -0700

    Update color config from sass to css var

--- src/js/main.ts
+++ src/js/main.ts
@@ -6,61 +6,73 @@ import "../style/style.scss";
 import { ProgramCore } from "./programExe";
 
 export const GLOBAL_CONFIG: {
-    desktop_mode: boolean,
-    simple_mode: boolean
+  desktop_mode: boolean;
+  simple_mode: boolean;
 } = {
-    desktop_mode: false,
-    simple_mode: false
+  desktop_mode: false,
+  simple_mode: false,
 };
 
 function createHTMLStructure(): HTMLElement {
-    const site_app = createHTMLElement('div', '', {id: 'site-app'});
+  const site_app = createHTMLElement("div", "", { id: "site-app" });
 
-    // third layer
-    const site_app_terminal_container = createHTMLElement('div','',{'class': 'terminal-container'});
-    const terminal_container_output = createHTMLElement('div', '', {'class': 'terminal-output'});
-    
-    site_app_terminal_container.appendChild(terminal_container_output);
-    site_app.appendChild(site_app_terminal_container);
+  // third layer
+  const site_app_terminal_container = createHTMLElement("div", "", {
+    class: "terminal-container",
+  });
+  const terminal_container_output = createHTMLElement("div", "", {
+    class: "terminal-output",
+  });
 
-    document.body.appendChild(site_app);
+  site_app_terminal_container.appendChild(terminal_container_output);
+  site_app.appendChild(site_app_terminal_container);
 
-    return site_app;
-}   
+  document.body.appendChild(site_app);
+
+  return site_app;
+}
 
 function main(): void {
-    // setup html structure
-    const site_app: HTMLElement = createHTMLStructure();
-    const site_semantic: HTMLElement = document.querySelector('#site-semantic')!;
+  // setup html structure
+  const site_app: HTMLElement = createHTMLStructure();
+  const site_semantic: HTMLElement = document.querySelector("#site-semantic")!;
 
-    // process url
-    const url: URL = new URL(window.location.href);
+  // process url
+  const url: URL = new URL(window.location.href);
 
-    const params = new Set((url.searchParams.get('options')) ? url.searchParams.get('options')!.split(','): []);
+  const params = new Set(
+    url.searchParams.get("options")
+      ? url.searchParams.get("options")!.split(",")
+      : []
+  );
 
-    GLOBAL_CONFIG.desktop_mode = (params.has('desktop')) ? true : false;  
-    GLOBAL_CONFIG.simple_mode = (params.has('simple')) ? true : false;   
+  GLOBAL_CONFIG.desktop_mode = params.has("desktop") ? true : false;
+  GLOBAL_CONFIG.simple_mode = params.has("simple") ? true : false;
 
-    if (GLOBAL_CONFIG.desktop_mode) {
-        site_app.classList.add('desktop');
-    }
+  // check desktop mode
+  if (GLOBAL_CONFIG.desktop_mode) {
+    site_app.classList.add("desktop");
+  }
 
-    if (GLOBAL_CONFIG.simple_mode) {
-        site_app.parentNode!.removeChild(site_app);
-        site_semantic.style.display = 'block';
-    } else {
-        site_semantic.parentNode!.removeChild(site_semantic);
-        // singletons
-        new ProgramCore();
-        new InputStream();
-        new OutputStreamScreen();
-        new DisplayController();
+  // check simple mode
+  if (GLOBAL_CONFIG.simple_mode) {
+    site_app.parentNode!.removeChild(site_app);
+    site_semantic.style.display = "block";
+    return;
+  } else {
+    site_semantic.parentNode!.removeChild(site_semantic);
+  }
 
-        // default
-        ProgramCore.getInstance().execute('home');
-    }
+  // start program
+
+  // singletons
+  new ProgramCore();
+  new InputStream();
+  new OutputStreamScreen();
+  new DisplayController();
+
+  // default
+  ProgramCore.getInstance().execute("home");
 }
 
 main();
-
-
